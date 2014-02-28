50cff550aed8fc7a3f7a37cc2230b07d8ed83cda
amitpatelinfo <amit.apatel@yahoo.com>
2014-02-28 15:25:06 +0530

    Update popular_tags_mapper.py

--- popular_tags/popular_tags_mapper.py
+++ popular_tags/popular_tags_mapper.py
@@ -15,6 +15,6 @@ for data in reader:
     if len(data) == 19:
         id, title, tagnames, author_id, body, node_type, parent_id, abs_parent_id, added_at, score, state_string, last_edited_id, last_activity_by_id, last_activity_at, active_revision_id, extra, extra_ref_id, extra_count, marked = data
         if node_type == "question":
-            tags = tagnames.split(" ")
+            tags = tagnames.strip().split(" ")
             for tag in tags:
                 print "{0}".format(tag)
